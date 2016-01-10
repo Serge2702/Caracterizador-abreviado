afc0347ab980f6c19ba04e8fa034a0f8e0ec459b
Serge2702 <sergiogmv2702@gmail.com>
2016-01-10 17:37:14 -0600

    Añadiendo conteos actuales

--- Script_clasificador_universal.lisp
+++ Script_clasificador_universal.lisp
@@ -7,28 +7,34 @@
 (cond
   ((equal nombre_archivo "Resultados_caracterizador/100-0.txt")
    (defparameter *conteo_caracteristicas*
-     (list (list 63 0)(list 33 0)(list 28 0)(list 17 2)(list 0 0)(list 0 0))))
+     (list (list 53 1)(list 32 0)(list 17 2)(list 0 0)(list 0 0)(list 0 0))))
   ((equal nombre_archivo "Resultados_caracterizador/10-0.txt")
    (defparameter *conteo_caracteristicas*
-     (list (list 37032 5445)(list 59898 2120)(list 26638 2439)(list 113447 3099)(list 0 0)(list 0 0))))
-  ((equal nombre_archivo "Resultados_caracterizador/200-0.txt")
-   (defparameter *conteo_caracteristicas*
-     (list (list 8 0)(list 0 0)(list 1 0)(list 0 0)(list 0 0)(list 0 0))))
+     (list (list 74285 9496)(list 30779 9262)(list 122132 10200)(list 0 0)(list 0 0)(list 0 0))))
   ((equal nombre_archivo "Resultados_caracterizador/20-0.txt")
    (defparameter *conteo_caracteristicas*
-     (list (list 6609 1342)(list 6573 282)(list 5940 227)(list 1694 825)(list 0 0)(list 0 0))))
+     (list (list 9749 1113)(list 7114 556)(list 2227 2126)(list 0 0)(list 0 0)(list 0 0))))
   ((equal nombre_archivo "Resultados_caracterizador/30-0.txt")
    (defparameter *conteo_caracteristicas*
-     (list (list 1821 498)(list 1388 39)(list 1447 91)(list 403 292)(list 0 0)(list 0 0))))
+     (list (list 2378 336)(list 1856 192)(list 612 685)(list 0 0)(list 0 0)(list 0 0))))
   ((equal nombre_archivo "Resultados_caracterizador/40-0.txt")
    (defparameter *conteo_caracteristicas*
-     (list (list 393 227)(list 541 9)(list 294 36)(list 163 146)(list 0 0)(list 0 0))))
+     (list (list 1015 135)(list 441 70)(list 257 285)(list 0 0)(list 0 0)(list 0 0))))
   ((equal nombre_archivo "Resultados_caracterizador/50-0.txt")
    (defparameter *conteo_caracteristicas*
-     (list (list 203 108)(list 273 5)(list 162 13)(list 87 87)(list 0 0)(list 0 0))))
+     (list (list 529 80)(list 248 28)(list 133 154)(list 0 0)(list 0 0)(list 0 0))))
   ((equal nombre_archivo "Resultados_caracterizador/60-0.txt")
    (defparameter *conteo_caracteristicas*
-     (list (list 141 48)(list 124 4)(list 86 7)(list 60 49)(list 0 0)(list 0 0))))
+     (list (list 264 32)(list 121 9)(list 78 62)(list 0 0)(list 0 0)(list 0 0))))
+  ((equal nombre_archivo "Resultados_caracterizador/70-0.txt")
+   (defparameter *conteo_caracteristicas*
+     (list (list 153 22)(list 97 3)(list 54 48)(list 0 0)(list 0 0)(list 0 0))))
+  ((equal nombre_archivo "Resultados_caracterizador/80-0.txt")
+   (defparameter *conteo_caracteristicas*
+     (list (list 93 10)(list 71 0)(list 33 27)(list 0 0)(list 0 0)(list 0 0))))
+  ((equal nombre_archivo "Resultados_caracterizador/90-0.txt")
+   (defparameter *conteo_caracteristicas*
+     (list (list 72 5)(list 50 0)(list 22 12)(list 0 0)(list 0 0)(list 0 0))))
   (t (print "Escribiste mal el nombre...")))
 
 (let ((temporal nil)(contador 0))
